e650892156f3eb2a09dc92f017639c62784e49f7
kh618 <khaled.abdelnabi.18@gmail.com>
2023-04-10 22:45:07 +0200

    init project

---
+++ src/main/java/com/abdelnabi618/uprivacy/UPrivcayApplication.java
@@ -0,0 +1,13 @@
+package com.abdelnabi618.uprivacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPrivcayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UPrivcayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdelnabi618/uprivacy/UPrivcayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelnabi618.uprivacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPrivcayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
